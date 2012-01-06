24b1f70149076e42d67d8a755c3fba14553066f1
Shreyas Chand <shreyas.chand@gmail.com>
2012-01-06 16:55:08 -0500

    initial commit

---
+++ src/com/shreyaschand/MEDIC/MEDICActivity.java
@@ -0,0 +1,13 @@
+package com.shreyaschand.MEDIC;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MEDICActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
