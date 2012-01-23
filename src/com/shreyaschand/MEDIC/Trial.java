7866397c05e21f19ab5529fbe0ed81fdce2d5f75
Shreyas Chand <shreyas.chand@gmail.com>
2012-01-22 19:28:36 -0500

    Added AsynTask for BT connection.

--- src/com/shreyaschand/MEDIC/Trial.java
+++ src/com/shreyaschand/MEDIC/Trial.java
@@ -1,18 +1,26 @@
 package com.shreyaschand.MEDIC;
 
+import java.lang.reflect.Method;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class Trial extends Activity implements OnClickListener {
 	
 	private BluetoothAdapter btAdapter;
+
+	public BluetoothSocket socket;
 
 	private static final int DEVICE_SELECT = 1;
 	private static final int REQUEST_ENABLE_BT = 2;
@@ -62,9 +70,33 @@ public class Trial extends Activity implements OnClickListener {
 			}
 			break;
 		case DEVICE_SELECT:
-			//connect device
+			new connectBT().execute(new String[] {data.getExtras().getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS)});
 			break;
 		}
+	}
+	
+	private class connectBT extends AsyncTask<String, Void, Boolean> {
+
+		protected Boolean doInBackground(String... mac) {
+			try {
+				BluetoothDevice device = btAdapter.getRemoteDevice(mac[0]);
+				Method m = device.getClass().getMethod("createRfcommSocket", new Class[] { int.class });
+				socket = (BluetoothSocket) m.invoke(device, 1);
+				socket.connect();
+			return true;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+		protected void onPostExecute(Boolean result) {
+			if(result) {
+				((TextView)findViewById(R.id.test_output)).setText("connected.");
+				findViewById(R.id.trial_connect_button).setEnabled(false);
+			}else{
+				((TextView)findViewById(R.id.test_output)).setText("error connecting.");
+			}
+		}
+		
 	}
 
 	public void onClick(View v) {
