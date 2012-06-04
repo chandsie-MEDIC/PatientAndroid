b8c967f71cf2ea86e12e918b88c0721aaedc8589
Shreyas Chand <shreyas.chand@gmail.com>
2012-06-04 02:19:16 -0400

    Eh, older model seemed to be working better.

--- src/com/shreyaschand/MEDIC/Patient/Doctor.java
+++ src/com/shreyaschand/MEDIC/Patient/Doctor.java
@@ -183,16 +183,14 @@ public class Doctor extends Activity implements OnClickListener {
 				BufferedReader in = new BufferedReader(new InputStreamReader(btSocket.getInputStream()));
 				long startTime = System.currentTimeMillis();
 				long time = System.currentTimeMillis();
-				StringBuilder message = new StringBuilder(20000); 
 				String input = in.readLine();
 				while (input != null && (time - startTime) < 5000) {
 					time = System.currentTimeMillis();
-					publishProgress(new String[] { time - startTime + ": " + input + "\n"});
-					message.append(time - startTime + ":" + input + "$");
+					publishProgress(new String[] { time - startTime + ": " + input});
+					docSocket.writer.println(input);
+					docSocket.writer.flush();
 					input = in.readLine();
 				}
-				docSocket.writer.println(message);
-				docSocket.writer.flush();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
